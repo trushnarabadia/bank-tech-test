a548d561889e5948763cd2bb88d1d9089d88201c
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-03-29 16:33:43 +0100

    withdraw method reduces balance

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -11,6 +11,7 @@ class BankAccount
   end
 
   def withdraw(amount)
+    @balance -= amount
   end
 
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -20,5 +20,12 @@ describe BankAccount do
       bank_account.deposit(75)
       expect(bank_account).to respond_to(:withdraw)
     end
+
+    it 'should reduce the balance of an account when a withdrawal is made' do 
+      bank_account = BankAccount.new
+      bank_account.deposit(75)
+      bank_account.withdraw(35)
+      expect(bank_account.balance).to eq (40)
+    end
   end
 end
