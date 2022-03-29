033306d9cf1091bd9688230832b5cfc63f4e6011
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-03-29 18:07:40 +0100

    rspec tests refactored

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,40 +1,40 @@
 require_relative '../lib/bank_account'
 
 describe BankAccount do 
+
+  before(:each) do
+    @bank_account = BankAccount.new
+    @date = Time.now.strftime('%d/%m/%Y')
+  end
+
   describe '#deposit' do
     it 'should respond to a deposit' do 
-      bank_account = BankAccount.new
-      expect(bank_account).to respond_to(:deposit)
+      expect(@bank_account).to respond_to(:deposit)
     end
 
     it 'should increase the balance of an account when a deposit is made' do 
-      bank_account = BankAccount.new
-      bank_account.deposit(100)
-      expect(bank_account.balance).to eq (100)
+      @bank_account.deposit(100)
+      expect(@bank_account.balance).to eq (100)
     end
 
     it 'should record the details of the deposit transaction' do 
-      bank_account = BankAccount.new
-      bank_account.deposit(15)
-      @date = Time.now.strftime('%d/%m/%Y')
+      @bank_account.deposit(15)
       info = { date: @date, amount: 15, balance: 15 } 
-      expect(bank_account.deposit_info(15)).to include (info)
+      expect(@bank_account.deposit_info(15)).to include (info)
     end
 
   end
 
   describe '#withdraw' do 
     it 'should respond to withdraw' do 
-      bank_account = BankAccount.new
-      bank_account.deposit(75)
-      expect(bank_account).to respond_to(:withdraw)
+      @bank_account.deposit(75)
+      expect(@bank_account).to respond_to(:withdraw)
     end
 
     it 'should reduce the balance of an account when a withdrawal is made' do 
-      bank_account = BankAccount.new
-      bank_account.deposit(75)
-      bank_account.withdraw(35)
-      expect(bank_account.balance).to eq (40)
+      @bank_account.deposit(75)
+      @bank_account.withdraw(35)
+      expect(@bank_account.balance).to eq (40)
     end
   end
 
